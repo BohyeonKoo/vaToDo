3f1ec4c33d95095e6f7e5cf4c0a67d509c86e40a
bohyeonkoo <koo@secondspace.kr>
2022-09-27 14:28:17 +0900

    MODIFY: structure modified

--- scripts/list.js
+++ scripts/list.js
@@ -1,127 +1,81 @@
 const targetBody = document.body;
-const listWrapper = targetBody.querySelector(".list-wrap");
-const addButton = document.querySelector(".add-btn");
-const saveButton = document.querySelector(".save-btn");
+const listWrap = targetBody.querySelector(".container > ul");
+const createForm = targetBody.querySelector(".container > form");
+
+const renderLists = (list) => {
+  if (list && list.length !== 0) {
+    const parsedList = list.map((el, idx) =>
+      `<li>
+        <div>
+          <h2 class=${el.type && el.type === "done" ? "done-title" : null}>${el.title ? el.title : "To Do"}</h2>
+          <span>${el.date ? "날짜: " + el.date : "date"}</span>
+        </div>
+        <div>
+          <button onclick="handleClearRemove(${idx}, 'clear')">${el.type ? el.type === "done" ? "해제" : "완료" : "완료"}</button>
+          <button onclick="handleClearRemove(${idx}, 'remove')">삭제</button>
+        </div>
+      </li>`
+    );
+    listWrap.insertAdjacentHTML('beforeend', parsedList);
+  } else {
+    listWrap.innerHTML = '<h1>리스트가 존재하지 않습니다.</h1>';
+  }
+};
+
+const handleClearRemove = (idx, type) => {
+  const currentList = JSON.parse(localStorage.getItem("currentList"));
+  let returnList = [];
+
+  if (type === "clear") {
+    returnList = currentList.map((item, itemIdx) => {
+      if (idx === itemIdx) item.type = item.type === "done" ? "todo" : "done";
+      return item;
+    });
+  } else {
+    let confirmResult = confirm("정말 삭제하시겠습니까?");
+    if (confirmResult) {
+      returnList = currentList.filter((item, itemIdx) => idx !== itemIdx);
+    } else {
+      returnList = currentList;
+    }
+  }
+
+  localStorage.setItem("currentList", JSON.stringify(returnList));
+  window.location.reload();
+}
 
 document.addEventListener("DOMContentLoaded", function () {
   const currentList = localStorage.getItem("currentList");
-
-  if (currentList) {
-    const parsedList = JSON.parse(currentList);
-    for (const list of parsedList) handleCreateTodo(list);
-  }
+  renderLists(JSON.parse(currentList));
 });
 
-addButton.onclick = function () {
-  const inputTag = listWrapper.getElementsByTagName("input");
-  // html에 DOM을 미리 만들어놓는게 좋아보임
-  if (inputTag.length === 0) {
-    handleCreateElement(
-      { domname: "input", alias: "newListItemWrapper" },
-      { domname: "div", alias: "deleteContentWrap" },
-      { domname: "input", alias: "test" }
-    );
-    const newListItemWrapper = document.createElement("div");
-    const newListInput = document.createElement("input");
-    const newListButton = document.createElement("button");
-    const deleteContentWrap = document.createElement("div");
-
-    // newListItemWrapper.classList.add("item-wrap");
-    // newListItemWrapper.className="item-wrap";
-
-    // role
-    newListItemWrapper.setAttribute("class", "item-wrap");
-
-    newListButton.appendChild(document.createTextNode("확인"));
-    deleteContentWrap.appendChild(document.createTextNode("X"));
-
-    newListItemWrapper.appendChild(newListInput);
-    newListItemWrapper.appendChild(deleteContentWrap);
-    newListItemWrapper.appendChild(newListButton);
-    listWrapper.appendChild(newListItemWrapper);
-
-    newListButton.onclick = () => handleAddTodo(newListItemWrapper, inputTag);
-
-    // newListButton.addEventListener("click", callback);
-
-    newListInput.onkeydown = (e) => {
-      if (e.key === "Enter") handleAddTodo(newListItemWrapper, inputTag);
-    };
-
-    deleteContentWrap.onclick = function () {
-      newListInput.value = "";
-    };
-  } else {
-    alert("이미 작성 중인 리스트가 존재합니다.");
-  }
-};
-
-saveButton.onclick = function () {
-  const alignList = document.getElementsByTagName("h2");
-  const convertListTitle = [];
-
-  for (const item of alignList) {
-    convertListTitle.push(item.textContent);
-  }
-  localStorage.setItem("currentList", JSON.stringify(convertListTitle));
-};
-
-function handleAddTodo(newListItemWrapper, inputTag) {
-  const inputVal = inputTag[0].value;
-
-  if (inputVal.trim() !== "") handleCreateTodo(inputVal, newListItemWrapper);
-  else alert("입력하실 내용을 재확인 해주세요.");
+const handleAddTodo = () => {
+  createForm.style.display = "flex";
 }
 
-function handleCreateTodo(inputVal, newListItemWrapper) {
-  const todoWrap = document.createElement("div");
-  const todoTitle = document.createElement("h2");
-  const buttonWrap = document.createElement("div");
-  const todoCheckButton = document.createElement("button");
-  const todoRemoveButton = document.createElement("button");
+const handleFormAction = (type) => {
+  if (type === 'add') {
+    const inputs = createForm.getElementsByTagName("input");
+    if (inputs[0].value && inputs[1].valueAsNumber) {
+      const title = inputs[0].value;
+      const date = inputs[1].valueAsNumber;
+      const type = "todo"
 
-  todoTitle.innerText = inputVal;
-  todoCheckButton.innerText = "완료";
-  todoRemoveButton.innerText = "삭제";
-
-  buttonWrap.appendChild(todoCheckButton);
-  buttonWrap.appendChild(todoRemoveButton);
-  todoWrap.appendChild(todoTitle);
-  todoWrap.appendChild(buttonWrap);
-  todoWrap.setAttribute("class", "todo-wrap");
-
-  if (newListItemWrapper) {
-    listWrapper.replaceChild(todoWrap, newListItemWrapper);
-  } else {
-    listWrapper.appendChild(todoWrap);
-  }
-
-  todoRemoveButton.onclick = function () {
-    listWrapper.removeChild(todoWrap);
-  };
-
-  todoCheckButton.onclick = function (e) {
-    if (e.target.textContent === "완료") {
-      todoTitle.setAttribute("class", "checked");
-      todoCheckButton.innerText = "해제";
+      const currentList = JSON.parse(localStorage.getItem("currentList"));
+      let convertTodo = currentList ? [...currentList, { title, date, type }] : [{ title, date, type }];
+      localStorage.setItem("currentList", JSON.stringify(convertTodo));
+      window.location.reload();
+      createForm.style.display = "none";
     } else {
-      todoTitle.setAttribute("class", null);
-      todoCheckButton.innerText = "완료";
+      alert("필수 값이 입력되지 않았습니다.")
     }
-  };
-}
-
-function handleCreateElement(...props) {
-  const obj = {};
-  props.forEach((item, index) => {
-    const key = item.alias ?? `${item}${index}`;
-    Object.assign(obj, { [key]: document.createElement(item.domname) });
-  });
-
-  return obj;
+  } else {
+    createForm.style.display = "none";
+  }
 }
 
 const handleSignOut = () => {
   localStorage.setItem("isSign", false);
   window.location.href = "../index.html";
 };
+
--- scripts/signIn.js
+++ scripts/signIn.js
@@ -13,9 +13,11 @@ const handleLoginClicekd = () => {
         localStorage.setItem("isSign", true);
         window.location.href = "list.html";
       } else {
-        console.log("asd")
         alert("일치하는 정보가 없습니다.");
       }
+    } else {
+      alert("가입된 정보가 없습니다. 회원가입을 진행해주세요");
+      window.location.href = "signUp.html";
     }
   } else {
     alert("미입력된 필수값이 존재합니다.");
